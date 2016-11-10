fa2d7595f854888a9fdac689f5b652f07a906999
Chris Zubak-Skees <chriszs@gmail.com>
2016-11-10 18:03:53 -0500

    Download 2014-15 school geodata

--- src/data/download.sh
+++ src/data/download.sh
@@ -1,3 +1,1 @@
-wget http://nces.ed.gov/ccd/Data/zip/sc132a_txt.zip
-wget http://nces.ed.gov/ccd/Data/zip/Sch14pre_txt.zip
-wget http://www.fhwa.dot.gov/planning/processes/tools/nhpn/2015/nhpnv14-05shp.zip
+wget -N http://nces.ed.gov/ccd/Data/zip/EDGE_GEOIDS_201415_PUBLIC_SCHOOL_csv.zip
