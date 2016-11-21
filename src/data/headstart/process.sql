994c2651261e6d6e5dfd0471f5b9f328c894c372
Chris Zubak-Skees <chriszs@gmail.com>
2016-11-21 17:35:14 -0500

    Update

--- src/data/headstart/process.sql
+++ src/data/headstart/process.sql
@@ -1,11 +1,11 @@
-ALTER TABLE publicschools1415 ADD COLUMN geom geography(Point,4326);
+ALTER TABLE headstart16 ADD COLUMN geom geography(Point,4326);
 
-UPDATE publicschools1415 SET geom = ST_SetSRID(ST_MakePoint(cast(longcode AS double PRECISION),cast(latcode AS double PRECISION)),4326);
+UPDATE headstart16 SET geom = ST_SetSRID(ST_MakePoint(cast(longitude AS double PRECISION),cast(latitude AS double PRECISION)),4326);
 
-CREATE INDEX ON publicschools1415 USING GIST (geom);
+CREATE INDEX ON headstart16 USING GIST (geom);
 
-ALTER TABLE roadswithtraffic14 ALTER COLUMN geom TYPE geography(multilinestring,4326) using ST_Force_2D(geom);
+# ALTER TABLE roadswithtraffic14 ALTER COLUMN geom TYPE geography(multilinestring,4326) using ST_Force_2D(geom);
 
-SELECT ncessch, latcode as latitude, longcode as longitude, gid, route_id, route_numb, state_code, through_la, f_system, aadt, aadt_combi, aadt_singl, ST_Distance(publicschools1415.geom,roadswithtraffic14.geom) as distance INTO roadsnearpublicschools1415 FROM publicschools1415, roadswithtraffic14 WHERE ST_DWithin(roadswithtraffic14.geom, publicschools1415.geom, 500);
+SELECT headstart16.id, latitude, longitude, gid, route_id, route_numb, state_code, through_la, f_system, aadt, aadt_combi, aadt_singl, ST_Distance(headstart16.geom,roadswithtraffic14.geom) as distance INTO roadsnearheadstart16 FROM headstart16, roadswithtraffic14 WHERE ST_DWithin(roadswithtraffic14.geom, headstart16.geom, 500);
 
-\copy roadsnearpublicschools1415 to '/Users/cskees/Desktop/highway-schools/src/data/roadsnearpublicschools1415.csv' with CSV HEADER;
+\copy roadsnearheadstart16 to '/Users/cskees/Desktop/highway-schools/src/data/roadsnearheadstart16.csv' with CSV HEADER;
