1fcae190b93d4edebc717035eada3a58b40cc861
Chris Zubak-Skees <chriszs@gmail.com>
2017-02-10 17:58:09 -0500

    Hopefully fix Mapnik choking on empty geometrycollections once and for all

--- src/routes/roads.js
+++ src/routes/roads.js
@@ -21,18 +21,19 @@ const buffer = 152.4;
 
 ts.layer('roads', (tile, render) => {
     render({
-        hightraffic: 'SELECT ST_AsGeoJSON(ST_Union(ST_Intersection(ST_Buffer(geom,' + buffer + ')::geometry,!bbox_4326!))) AS the_geom_geojson ' +
+        hightraffic: 'SELECT CASE WHEN ST_IsEmpty(geom) THEN null ELSE ST_AsGeoJSON(geom) END AS the_geom_geojson ' +
+            'FROM (SELECT ST_Union(ST_Intersection(ST_Buffer(geom,' + buffer + ')::geometry,!bbox_4326!)) as geom ' +
             'FROM roadswithtraffic14 ' +
-            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0011)) ' +
-            'AND aadt >= 30000',
+            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0015)) ' +
+            'AND aadt >= 30000) as hightraffic',
         truckroute: 'SELECT CASE WHEN ST_IsEmpty(ST_Difference(truckroute.geom,hightraffic.geom)) THEN null ELSE ST_AsGeoJSON(COALESCE(ST_Difference(truckroute.geom,hightraffic.geom),truckroute.geom)) END AS the_geom_geojson ' +
             'FROM (SELECT ST_Union(ST_Intersection(ST_Buffer(geom,' + buffer + ')::geometry,!bbox_4326!)) AS geom ' +
             'FROM roadswithtraffic14 ' +
-            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0011)) ' +
+            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0015)) ' +
             'AND aadt >= 30000) as hightraffic, ' +
             '(SELECT ST_Union(ST_Intersection(ST_Buffer(geom,' + buffer + ')::geometry,!bbox_4326!)) AS geom ' +
             'FROM roadswithtraffic14 ' +
-            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0011)) ' +
+            'WHERE ST_Intersects(geom, ST_Expand(!bbox_4326!,0.0015)) ' +
             'AND aadt >= 10000 AND aadt_combi+aadt_singl >= 500 AND aadt < 30000) as truckroute'
     });
 });
